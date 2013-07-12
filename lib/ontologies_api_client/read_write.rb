424ae83ca3a268c14ca07669441fb9566e1b3865
Paul R Alexander <palexander@stanford.edu>
2013-07-12 11:41:53 -0700

    Move HTTP out of global scope

--- lib/ontologies_api_client/read_write.rb
+++ lib/ontologies_api_client/read_write.rb
@@ -1,8 +1,8 @@
-HTTP = LinkedData::Client::HTTP
-
 module LinkedData
   module Client
     module ReadWrite
+      HTTP = LinkedData::Client::HTTP
+
       def save
         # Create via post
         HTTP.post(self.class.collection_path, self.to_hash)
