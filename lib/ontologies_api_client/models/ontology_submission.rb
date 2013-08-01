8f7c8af8fc6436d7b42f94d0675caf7a4b19e811
Paul R Alexander <palexander@stanford.edu>
2013-08-01 15:41:56 -0700

    Submissions are read/write

--- lib/ontologies_api_client/models/ontology_submission.rb
+++ lib/ontologies_api_client/models/ontology_submission.rb
@@ -5,6 +5,8 @@ module LinkedData
     module Models
       class OntologySubmission < LinkedData::Client::Base
         include LinkedData::Client::Collection
+        include LinkedData::Client::ReadWrite
+        
         @media_type = "http://data.bioontology.org/metadata/OntologySubmission"
         @include_attrs    = "all"
       end
