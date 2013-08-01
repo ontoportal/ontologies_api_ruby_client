0118b25446c550d8def2d37d25f82412ec43d8ce
Paul R Alexander <palexander@stanford.edu>
2013-08-01 15:42:08 -0700

    Add update method to read/write objects

--- lib/ontologies_api_client/read_write.rb
+++ lib/ontologies_api_client/read_write.rb
@@ -8,6 +8,26 @@ module LinkedData
         HTTP.post(self.class.collection_path, self.to_hash)
       end
       
+      def update
+        existing = HTTP.get(self.id)
+        changed_attrs = {}
+        existing.instance_variables.each do |var|
+          var_sym = var[1..-1].to_sym
+          next if [:links, :context].include?(var_sym)
+          current_value = self.instance_variable_get(var)
+          existing_value = existing.instance_variable_get(var)
+          changed_attrs[var_sym] = current_value if current_value != existing_value
+        end
+        HTTP.patch(self.id, changed_attrs)
+      end
+      
+      def update_from_params(params)
+        params.each do |k,v|
+          self.send("#{k}=", v) rescue next
+        end
+        self.update
+      end
+      
       def delete
         HTTP.delete(self.id)
       end
