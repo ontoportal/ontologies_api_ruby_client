2844f5cd50a6187d6df8d08ee67e7b9b8e9eb662
Paul R Alexander <palexander@stanford.edu>
2013-08-05 17:01:58 -0700

    Make user read/write

--- lib/ontologies_api_client/models/user.rb
+++ lib/ontologies_api_client/models/user.rb
@@ -6,11 +6,13 @@ module LinkedData
     module Models
       class User < LinkedData::Client::Base
         include LinkedData::Client::Collection
+        include LinkedData::Client::ReadWrite
+        
         @media_type = "http://data.bioontology.org/metadata/User"
         @include_attrs    = "all"
         
         def self.authenticate(user, password)
-          auth_params = {user: user, password: password}
+          auth_params = {user: user, password: password, include: "all"}
           LinkedData::Client::HTTP.post("#{LinkedData::Client.settings.rest_url}users/authenticate", auth_params)
         end
         
