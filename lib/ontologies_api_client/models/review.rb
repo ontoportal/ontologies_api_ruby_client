586971cae58f35dc445f92dd555565d73d1afbd7
Paul R Alexander <palexander@stanford.edu>
2013-08-11 23:51:39 -0700

    Make review read/write

--- lib/ontologies_api_client/models/review.rb
+++ lib/ontologies_api_client/models/review.rb
@@ -5,6 +5,8 @@ module LinkedData
     module Models
       class Review < LinkedData::Client::Base
         include LinkedData::Client::Collection
+        include LinkedData::Client::ReadWrite
+        
         @media_type = "http://data.bioontology.org/metadata/Review"
       end
     end
