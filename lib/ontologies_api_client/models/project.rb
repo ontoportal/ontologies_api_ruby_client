6be9daab7eb6cd7d375884a1f1e3116d42b1708e
Paul R Alexander <palexander@stanford.edu>
2013-08-12 15:00:48 -0700

    Make project read/write

--- lib/ontologies_api_client/models/project.rb
+++ lib/ontologies_api_client/models/project.rb
@@ -5,6 +5,8 @@ module LinkedData
     module Models
       class Project < LinkedData::Client::Base
         include LinkedData::Client::Collection
+        include LinkedData::Client::ReadWrite
+
         @media_type = "http://data.bioontology.org/metadata/Project"
       end
     end
