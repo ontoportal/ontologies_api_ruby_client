0081bf6ad1785cd8f994144cfb3c0f8dc1619117
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2013-08-16 14:08:07 -0700

    Correct typo from copy/paste on metrics class.

--- lib/ontologies_api_client/models/metrics.rb
+++ lib/ontologies_api_client/models/metrics.rb
@@ -3,7 +3,7 @@ require_relative "../base"
 module LinkedData
   module Client
     module Models
-      class Review < LinkedData::Client::Base
+      class Metrics < LinkedData::Client::Base
         include LinkedData::Client::Collection
         
         @media_type = "http://data.bioontology.org/metadata/Metrics"
