fcada2f293ce89ba2101b4023edc444c6c125189
Paul R Alexander <palexander@stanford.edu>
2013-08-15 18:48:00 -0700

    Work around problem where a 304 is returned but the object is not in the faraday object cache

--- lib/ontologies_api_client/middleware/faraday-object-cache.rb
+++ lib/ontologies_api_client/middleware/faraday-object-cache.rb
@@ -20,22 +20,49 @@ module Faraday
           # Alter the cache request object for storing our parsed objects
           cache_request[:object_cached] = true
           last_modified = requested_env[:response_headers]["Last-Modified"]
+          # Access storage's private key generation method
           key = storage.send(:cache_key_for, cache_request)
           if storage.cache.exist?(key)
             stored_obj = storage.cache.read(key).dup
+
             # Update if last modified is different
             if stored_obj[:last_modified] != last_modified
               puts "UPDATING CACHE #{requested_env[:url].to_s}"
               stored_obj[:last_modified] = last_modified
               storage.cache.write(key, stored_obj)
             end
-            return stored_obj[:ld_obj]
+            
+            # If we have a string, we must have had to serialize to JSON to avoid Marshal failures (see below)
+            if stored_obj[:ld_obj].is_a?(String)
+              stored_obj[:ld_obj] = MultiJson.load(stored_obj[:ld_obj])
+            end
+            
+            ld_obj = stored_obj[:ld_obj]
           else
+            # We were encountering a weird error where responses with 304 weren't in
+            # the cache, so to prevent a failure if we have a 304 but end up here
+            # we are going to re-trigger the request
+            if requested_env[:status] == 304
+              puts "RETRYING QUERY (cache missed but we got a 304) #{requested_env[:url].to_s}"
+              env[:request_headers]["If-Modified-Since"] = nil
+              requested_env = @app.call(env)
+            end
+            
             ld_obj = LinkedData::Client::HTTP.object_from_json(requested_env[:body])
+            puts "STORING OBJECT: #{requested_env[:url].to_s}"
             stored_obj = {last_modified: last_modified, ld_obj: ld_obj}
-            storage.cache.write(key, stored_obj)
-            return ld_obj
+
+            begin
+              storage.cache.write(key, stored_obj)
+            rescue TypeError
+              # Could not serialize the ld_obj with Marshal (probably because it's a struct),
+              # convert to JSON string instead
+              stored_obj[:ld_obj] = MultiJson.dump(ld_obj)
+              storage.cache.write(key, stored_obj)
+            end
           end
+
+          return ld_obj
         end
       end
     end
