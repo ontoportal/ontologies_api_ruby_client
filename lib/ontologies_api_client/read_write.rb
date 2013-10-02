8aa737bc0408c32378cb54c2ba0d073992dd8663
Paul R Alexander <palexander@stanford.edu>
2013-10-02 11:04:45 -0700

    Major revision in how attributes are compared when updating an object

--- lib/ontologies_api_client/read_write.rb
+++ lib/ontologies_api_client/read_write.rb
@@ -15,6 +15,15 @@ module LinkedData
       end
       
       def update_from_params(params)
+        # We want to populate ALL the attributes from the REST
+        # service so we know that what we're updating is
+        # actually the full object
+        all_values = HTTP.get(self.id, include: "all")
+        all_values.instance_variables.each do |var|
+          self.send("#{var}=", all_values.instance_variable_get(var))
+        end
+
+        # Now we override the retrieved attributes with new ones
         params.each do |k,v|
           self.send("#{k}=", v) rescue next
         end
@@ -22,14 +31,14 @@ module LinkedData
       end
       
       def changed_values
-        existing = HTTP.get(self.id)
+        existing = HTTP.get(self.id, include: "all")
         changed_attrs = {}
         self.instance_variables.each do |var|
           var_sym = var[1..-1].to_sym
           next if [:id, :type, :links, :context, :created].include?(var_sym)
-          current_value = self.instance_variable_get(var)
-          existing_value = existing.instance_variable_get(var)
-          changed_attrs[var_sym] = current_value if current_value != existing_value
+          new_value = self.instance_variable_get(var)
+          current_value = existing.instance_variable_get(var)
+          changed_attrs[var_sym] = new_value unless equivalent?(current_value, new_value)
         end
         changed_attrs
       end
@@ -37,6 +46,33 @@ module LinkedData
       def delete
         HTTP.delete(self.id)
       end
+      
+      private
+      
+      def equivalent?(current_value, new_value)
+        # If we're comparing an existing embedded object
+        # then use the id for comparison
+        if current_value.is_a?(LinkedData::Client::Base)
+          return current_value.id.eql?(new_value)
+        end
+
+        # Otherwise, do some complex comparing
+        case new_value
+        when String
+          return current_value.to_s.eql?(new_value)
+        when Array, Hash
+          new_value = nil if new_value.is_a?(Array) && new_value.empty? && current_value.nil?
+          if new_value.is_a?(Array) && new_value.first.is_a?(Hash)
+            clean_current = current_value.map {|e| e.to_h.symbolize_keys.delete_if {|k,v| v.nil?}}.sort
+            clean_new = new_value.map {|e| e.symbolize_keys.delete_if {|k,v| v.nil?}}.sort
+            return clean_current.eql?(clean_new) rescue clean_current == clean_new
+          else
+            return current_value.sort.eql?(new_value.sort) rescue current_value == new_value
+          end
+        end
+        return current_value.eql?(new_value) rescue current_value == new_value
+      end
+      
     end
   end
 end
