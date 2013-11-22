158b21120d280c9a033225257dcfcd8edda699b1
Paul R Alexander <palexander@stanford.edu>
2013-11-21 16:00:47 -0800

    Override methods in mappings to support weird mappings ids

--- lib/ontologies_api_client/models/mapping.rb
+++ lib/ontologies_api_client/models/mapping.rb
@@ -1,3 +1,4 @@
+require "cgi"
 require_relative "../base"
 
 module LinkedData
@@ -8,6 +9,27 @@ module LinkedData
         include LinkedData::Client::ReadWrite
 
         @media_type = "http://data.bioontology.org/metadata/Mapping"
+
+        def self.find(id, params = {})
+          HTTP.get(mappings_url_prefix + CGI.escape(id), params)
+        end
+
+        def delete
+          HTTP.delete(mappings_url_prefix + CGI.escape(self.id))
+        end
+
+        private
+
+        ##
+        # This is in a method because the settings are configured after
+        # the VM initialization, so the rest_url could be null or wrong
+        def self.mappings_url_prefix
+          LinkedData::Client.settings.rest_url + "/mappings/"
+        end
+
+        def mappings_url_prefix
+          self.class.mappings_url_prefix
+        end
       end
     end
   end
