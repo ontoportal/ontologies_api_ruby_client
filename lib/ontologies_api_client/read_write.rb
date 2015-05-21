2af7937f07a529486eb188847aad4ddaa3f92bbd
Paul R Alexander <palexander@stanford.edu>
2015-05-21 14:53:41 -0700

    Typo

--- lib/ontologies_api_client/read_write.rb
+++ lib/ontologies_api_client/read_write.rb
@@ -84,7 +84,7 @@ module LinkedData
         self.class.all(invalidate_cache: true)
         HTTP.get(self.id, invalidate_cache: true) if self.id
         session = Thread.current[:session]
-        session[:last_update] = Time.now.to_f if session
+        session[:last_updated] = Time.now.to_f if session
         refresh_cache
       end
 
