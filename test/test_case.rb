6f51466689eb03fe6f8ca9db8d1a5c15da02cdb1
Jennifer Vendetti <vendetti@stanford.edu>
2023-07-19 17:29:12 -0700

    Modify unit test code to use Minitest

--- test/test_case.rb
+++ test/test_case.rb
@@ -1,10 +1,10 @@
-require 'test-unit'
+require 'minitest/autorun'
 require_relative '../lib/ontologies_api_client'
 require_relative '../config/config'
 
 module LinkedData
   module Client
-    class TestCase < Test::Unit::TestCase
+    class TestCase < Minitest::Test
     end
   end
 end
